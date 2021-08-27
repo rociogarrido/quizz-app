a77d4e3967da22d74e6a96c67749ed85621e4c02
Rocío Garrido <rocioganon@gmail.com>
2021-08-27 16:35:41 +0200

    add hooks in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 // components
 import QuestionCard from './components/QuestionCard';
 
 const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
 
   const startTrivia = async () => {
   }
@@ -21,7 +28,7 @@ const App = () => {
       <button className="start" onClick={startTrivia}>Start</button>
       <p className="score">Score:</p>
       <p>Loading Questions ...</p>
-      <QuestionCard/>
+      <QuestionCard questionNumber={number + 1}/>
         <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
