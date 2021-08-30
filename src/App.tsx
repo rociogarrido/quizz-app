3f8810a4fa9d73c2b38d7949ccd7e9a80ff34df2
Rocío Garrido <rocioganon@gmail.com>
2021-08-30 15:14:08 +0200

    add checkAnswer function

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,23 @@ const App = () => {
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-  }
+    if (!gameOver) {
+      // user answer
+      const answer = e.currentTarget.value;
+      // check answer against correct answer
+      const correct = questions[number].correct_answer === answer;
+      // add score if answer is correct
+      if (correct) setScore(prev => prev + 1);
+      // save answer in the array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      }
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  };
 
   const nextQuestion = () => {
   }
@@ -56,15 +72,20 @@ const App = () => {
 
       {!gameOver ? <p className="score">Score:</p> : null}
       {loading ? <p>Loading Questions ...</p> : null}
-      {/*<QuestionCard 
+      
+      {!loading && !gameOver && (
+      <QuestionCard 
       questionNumber={number + 1} 
       totalQuestions={TOTAL_QUESTIONS} 
       question={questions[number].question} 
       answers={questions[number].answers} 
       userAnswer={userAnswers ? userAnswers[number] : undefined} 
       callback={checkAnswer} 
-      /> */}
+      />
+      )}
+      {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
         <button className="next" onClick={nextQuestion}>Next Question</button>
+      ) : null} 
     </div>
   );
 }
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -17,7 +17,7 @@ const QuestionCard: React.FC<Props> = ({question, answers, callback, userAnswer,
     <p dangerouslySetInnerHTML={{ __html: question }} />
     <div>
       {answers.map((answer) => (
-        <div>
+        <div key={answer}>
           <button disabled={!!userAnswer} value={answer} onClick={callback}>
             <span dangerouslySetInnerHTML={{ __html: answer }} />
           </button>
