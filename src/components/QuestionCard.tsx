c0a230d5a9d351947e69d17cb04aaea6b75461a7
Rocío Garrido <rocioganon@gmail.com>
2021-08-30 16:25:32 +0200

    import Wrapper and ButtonWrapper in QuestionCard component

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // types
 import { AnswerObject } from '../App';
 // styles
-// import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
 
 type Props = {
   question: string;
@@ -14,21 +14,25 @@ type Props = {
 };
 
 const QuestionCard: React.FC<Props> = ({question, answers, callback, userAnswer, questionNumber, totalQuestions }) => (
-  <div>
+  <Wrapper>
     <p className="number">
       Question: {questionNumber} / {totalQuestions}
     </p>
     <p dangerouslySetInnerHTML={{ __html: question }} />
     <div>
       {answers.map((answer) => (
-        <div key={answer}>
+        <ButtonWrapper 
+          key={answer} 
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer} 
+        >
           <button disabled={!!userAnswer} value={answer} onClick={callback}>
             <span dangerouslySetInnerHTML={{ __html: answer }} />
           </button>
-        </div>
+        </ButtonWrapper>
       ))}
   </div>
-  </div>
+  </Wrapper>
 );
 
 export default QuestionCard;
